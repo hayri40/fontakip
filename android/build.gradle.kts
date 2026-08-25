88f061de11d5309be0599173648077085416ea84
Hayri <mehmethayri40@gmail.com>
2026-08-25 23:48:15 +0300

    release: v0.8.0-beta

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,6 +19,26 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+subprojects {
+    afterEvaluate {
+        val project = this
+        if (project.extensions.findByName("android") != null) {
+            project.extensions.configure<com.android.build.gradle.BaseExtension> {
+                buildTypes.forEach { buildType ->
+                    // Replace the deprecated proguard-android.txt with proguard-android-optimize.txt
+                    val files = buildType.proguardFiles
+                    if (files.any { it.name == "proguard-android.txt" }) {
+                        buildType.setProguardFiles(listOf(
+                            project.android.getDefaultProguardFile("proguard-android-optimize.txt"),
+                            "proguard-rules.pro"
+                        ))
+                    }
+                }
+            }
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
